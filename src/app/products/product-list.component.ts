9334692b59cab6af708d2a7e6eae2e75ef930526
MALAJ Mark <mark.malaj@soprasteria.com>
2019-10-03 14:53:41 +0200

    chore: create http products service - Observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -23,6 +23,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -50,8 +51,13 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this.ProductService.getProducts();
-        this.filteredProducts = this.products;
+        this.ProductService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err 
+        });
     }
 
     onRatingClicked(message: string): void {
