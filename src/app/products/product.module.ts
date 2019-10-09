ed5b6e2b8f8d29a4e29e5438bba4073918454a75
MALAJ Mark <mark.malaj@soprasteria.com>
2019-10-09 10:30:43 +0200

    refactor: create new feature module

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-space.pipe';
+import { StarComponent } from '../shared/star.compenent';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-detail.guard';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { 
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      },
+    ])
+  ]
+})
+export class ProductModule { }
